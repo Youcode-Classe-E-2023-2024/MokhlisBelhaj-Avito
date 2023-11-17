a4bb0e22897750506ac1ee2cc2d4e4390dae406b
BelhajMokhlis <belhajmokhlis@gmail.com>
2023-11-17 14:57:31 +0100

    baseController

---
+++ app/libraries/controller.php
@@ -0,0 +1,23 @@
+<?php
+class controller{
+    // load model 
+    public function model($model){
+        // Require model file
+        require_once '../app/models/'.$model.'.php';
+
+        // instatait model
+        return new $model();
+
+    }
+    // load views
+    public function view($view,$data=[]){
+    // check for view file 
+     if(file_exists('../app/views/'.$view.'.php')){
+        require_once '../app/views/'.$view.'.php';
+     }else{
+        // view does not exist
+        die('view does not exist');
+     }
+
+    }
+}
